3b9620d0ec12d17932eda7ed61d0ef6bcf901fff
sbrommer <sbrommer@users.noreply.github.com>
2021-04-28 15:41:46 +0200

    karte

---
+++ karte.py
@@ -0,0 +1,29 @@
+from sys import stdin
+
+line = stdin.readline()
+
+n = len(line) // 3
+
+deck = {'P' : set(),
+        'K' : set(),
+        'H' : set(),
+        'T' : set()}
+
+double = False
+
+for i in range(n):
+    suit = line[i*3]
+    number = int(line[i*3+1:(i+1)*3])
+
+    if number in deck[suit]:
+        double = True
+        break
+
+    else:
+        deck[suit].add(number)
+
+if double:
+    print('GRESKA')
+else:
+    for suit in 'PKHT':
+        print(13 - len(deck[suit]), end=' ')
