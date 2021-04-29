248bc425ee218e7ada5316d4dead3593def43187
sbrommer <sbrommer@users.noreply.github.com>
2021-04-29 15:33:52 +0200

    musicalscales

---
+++ musicalscales.py
@@ -0,0 +1,17 @@
+from sys import stdin
+
+stdin.readline()
+song = set(stdin.readline().split())
+
+notes = ['A', 'A#', 'B', 'C', 'C#', 'D',
+         'D#', 'E', 'F', 'F#', 'G', 'G#']
+
+major = [0,2,4,5,7,9,11]
+
+scales = []
+for n in range(12):
+    scale = {notes[(m + n) % 12] for m in major}
+    if song <= scale:
+        scales.append(notes[n])
+
+print(*scales or ['none'])
