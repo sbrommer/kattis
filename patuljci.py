7b15170c3dee7becdac300beb56411ade672d30e
sbrommer <sbrommer@users.noreply.github.com>
2021-04-29 11:29:07 +0200

    patuljci

---
+++ patuljci.py
@@ -0,0 +1,15 @@
+from sys import stdin
+
+d_list = []
+
+for _ in range(9):
+    d_list.append(int(stdin.readline()))
+
+d_set = set(d_list)
+
+n = sum(d_set) - 100
+
+for d1 in d_list:
+    d2 = n - d1
+    if d1 == d2 or d2 not in d_set:
+        print(d1)
