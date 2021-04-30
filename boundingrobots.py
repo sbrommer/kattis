1aa85f486f2353a99899064bc897cacd25a99081
sbrommer <sbrommer@users.noreply.github.com>
2021-04-30 13:12:44 +0200

    refactor boundingrobots

--- boundingrobots.py
+++ boundingrobots.py
@@ -3,35 +3,26 @@ from sys import stdin
 def readints():
     return list(map(int, stdin.readline().split()))
 
-w, l = readints()
+room = readints()
 
-while [w, l] != [0, 0]:
-    robot = [0, 0]
-    actually = [0, 0]
+while room != [0, 0]:
+    rob = [0, 0]
+    act = [0, 0]
 
     n = int(stdin.readline())
 
     for _ in range(n):
         d, m = stdin.readline().split()
-        m = int(m)
 
-        if d == 'd' or d == 'l':
-            m *= -1
+        m = int(m) * (1 - 2 * int(d in 'dl'))
+        i = d in 'du'
 
-        if d == 'l' or d == 'r':
-            robot[0]    += m
-            actually[0] += m
+        rob[i] += m
+        act[i] += m
+        act[i] = max(min(act[i], room[i] - 1), 0)
 
-            actually[0] = max(min(actually[0], w - 1), 0)
-
-        if d == 'd' or d == 'u':
-            robot[1]    += m
-            actually[1] += m
-
-            actually[1] = max(min(actually[1], l - 1), 0)
-
-    print('Robot thinks', robot[0], robot[1])
-    print('Actually at', actually[0], actually[1])
+    print('Robot thinks', rob[0], rob[1])
+    print('Actually at', act[0], act[1])
     print()
 
-    w, l = readints()
+    room = readints()
