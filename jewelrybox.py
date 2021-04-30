20be4e1cc6ac18b21edc914933df5c0b93a1bc69
sbrommer <sbrommer@users.noreply.github.com>
2021-04-30 11:01:11 +0200

    jewelrybox

---
+++ jewelrybox.py
@@ -0,0 +1,22 @@
+from sys import stdin
+from math import sqrt
+
+T = int(stdin.readline())
+
+for _ in range(T):
+    X, Y = list(map(int, stdin.readline().split()))
+
+    # Calculate h that gives the biggest volume.
+    A = 12
+    B = -4 * (X + Y)
+    C = X*Y
+    D = B ** 2 - 4 * A * C
+
+    h = (-B - sqrt(D)) / (2 * A)
+
+    # Calculate volume.
+    a = X - 2 * h
+    b = Y - 2 * h
+    V = a * b * h
+
+    print(V)
