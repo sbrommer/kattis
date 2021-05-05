f50df06a9bed6ce19a49cc7c7aa12a5536c8956d
sbrommer <sbrommer@users.noreply.github.com>
2021-05-05 14:14:11 +0200

    cetiri

---
+++ cetiri.py
@@ -0,0 +1,15 @@
+from sys import stdin
+
+ns = list(map(int, stdin.readline().split()))
+
+ns.sort()
+
+d1 = ns[1] - ns[0]
+d2 = ns[2] - ns[1]
+
+if d1 < d2:
+    print(ns[1] + d1)
+elif d1 > d2:
+    print(ns[0] + d2)
+else:
+    print(ns[2] + d1)
