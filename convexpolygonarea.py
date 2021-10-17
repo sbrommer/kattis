dcc63bcba0860f21768b7b9678b7bc4c0c7dbfea
sbrommer <sbrommer@users.noreply.github.com>
2021-10-17 16:48:24 +0200

    convexpolygonarea

---
+++ convexpolygonarea.py
@@ -0,0 +1,15 @@
+from sys import stdin
+
+n = int(stdin.readline())
+
+for _ in range(n):
+    m, *cs = list(map(int, stdin.readline().split()))
+    x = cs[0::2]
+    y = cs[1::2]
+
+    area = 0
+    for i in range(m):
+        area += x[i] * y[(i + 1) % m]
+        area -= y[i] * x[(i + 1) % m]
+
+    print(0.5 * area)
