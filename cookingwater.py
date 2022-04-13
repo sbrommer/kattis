50fed2d134f46091a600c053f66d654b228afa85
Susan <sbrommer@users.noreply.github.com>
2022-04-13 13:43:45 +0200

    cookingwater

---
+++ cookingwater.py
@@ -0,0 +1,23 @@
+# parse input
+o = open(0)
+
+N = int(o.readline())
+
+
+def readints():
+    return list(map(int, o.readline().split()))
+
+
+# initialise
+min_boil = 0
+max_boil = 1000
+
+# get possible boiling minutes
+for _ in range(N):
+    a, b = readints()
+
+    min_boil = max(min_boil, a)
+    max_boil = min(max_boil, b)
+
+# answer
+print('gunilla has a point' if min_boil <= max_boil else 'edward is right')
