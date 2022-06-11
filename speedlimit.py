3513f8732567798fa1ced31074b0c4949fbca06c
Susan <sbrommer@users.noreply.github.com>
2022-06-11 08:37:12 +0200

    refactor speedlimit

--- speedlimit.py
+++ speedlimit.py
@@ -1,18 +1,20 @@
-import sys
+o = open(0)
 
-n = int(sys.stdin.readline())
+n = int(o.readline())
 
 while n >= 0:
-    d = 0
-    t_old = 0
+    # parse
+    ss, ts = [], []
 
     for _ in range(n):
-        line = sys.stdin.readline()
-        (s, t) = map(int, line.split())
-        d += s * (t - t_old)
-        t_old = t
+        s, t = [int(i) for i in o.readline().split()]
+        ss.append(s)
+        ts.append(t)
+
+    # calculate distance
+    ts = [t2 - t1 for t1, t2 in zip([0] + ts, ts)]
+    d = sum(s * t for s, t in zip(ss, ts))
 
     print(d, 'miles')
 
-    n = int(sys.stdin.readline())
-
+    n = int(o.readline())
