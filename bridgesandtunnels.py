2a8d41e8888ecf02120e4fd095eed247dabddcd5
Susan <sbrommer@users.noreply.github.com>
2023-11-12 18:42:11 +0100

    bridgesandtunnels

---
+++ bridgesandtunnels.py
@@ -0,0 +1,56 @@
+from itertools import groupby
+
+
+class DisjointSet:
+    parents = {}
+    depths = {}
+    sizes = {}
+
+    def __str__(self):
+        return str([set(g) for _, g in groupby(self.parents, self.find)])
+
+    def add(self, n):
+        if n not in self.parents:
+            self.parents[n] = n
+            self.depths[n] = 0
+            self.sizes[n] = 1
+
+    def find(self, n):
+        while self.parents[n] != n:
+            n, self.parents[n] = self.parents[n], self.parents[self.parents[n]]
+
+        return n
+
+    def union(self, a, b):
+        ra = self.find(a)
+        da = self.depths[ra]
+        rb = self.find(b)
+        db = self.depths[rb]
+
+        if ra != rb:
+            if da <= db:
+                da, db = db, da
+                ra, rb = rb, ra
+
+            self.parents[rb] = ra
+            if da == db:
+                self.depths[ra] = db + 1
+
+            # update size
+            self.sizes[ra] += self.sizes[rb]
+
+    def getset(self, n):
+        return set(m for m in self.parents if self.parents[m] == self.parents[n])
+
+
+
+ds = DisjointSet()
+
+n = int(input())
+
+for _ in range(n):
+    a, b = input().split()
+    ds.add(a)
+    ds.add(b)
+    ds.union(a, b)
+    print(ds.sizes[ds.find(a)])
