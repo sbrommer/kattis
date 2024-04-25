104a959f3461d92b0537ecad09baf6b19f5d3fbf
Susan <sbrommer@users.noreply.github.com>
2024-04-25 14:50:31 +0200

    radgreining1

--- radgreining1.py
+++ radgreining1.py
@@ -1,19 +1,19 @@
-def replace(string, s, section):
-    for i, letter in enumerate(section):
-        if string[s+i-1] not in ['?', letter]:
-            return 'Villa'
+def solve():
+    n, m = map(int, input().split())
 
-        string[s+i-1] = letter
+    string = ['?'] * n
 
-    return string
+    for _ in range(m):
+        s, section = input().split()
+        s, section = int(s), list(section)
+
+        for i, letter in enumerate(section, -1):
+            if string[s+i] not in ['?', letter]:
+                return 'Villa'
+
+            string[s+i] = letter
+
+    return ''.join(string)
 
 
-n, m = map(int, input().split())
-
-string = ['?'] * n
-
-for _ in range(m):
-    s, section = input().split()
-    string = replace(string, int(s), list(section))
-
-print(''.join(string))
+print(solve())
